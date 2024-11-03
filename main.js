132f909afe3fd40a528c48c0fa20eb9274b8cb77
quyen6 <quyenthai.060603@gmail.com>
2024-11-03 15:28:10 +0700

    callback

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-function sum(a, b){
-    return a+b;
+// function getName(x,y){
+//     return x+y;
+// }
+// console.log(`check: `, getName(5,6));
+///arrow function
+
+let sum = (a,b, callback)=> {
+    let tong = a+b;
+    setTimeout(()=> {
+        
+    }, 5000);
+    let i = 0 ;
+    let timer = setInterval(() => {
+        
+        callback(tong);
+        i++;
+        if (i===5 ){
+         clearInterval(timer);
+        }
+    }, 1000);
 }
+
+
+let printSum = (message)=> {
+    console.log(`check`, message)
+}
+sum(6,9, printSum)
